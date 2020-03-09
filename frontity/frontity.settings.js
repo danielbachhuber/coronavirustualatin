708664a98d531626b43bed85c87cc9b1a5578727
Daniel Bachhuber <daniel@handbuilt.co>
2020-03-09 10:49:00 -0700

    Use a dynamic API source

--- frontity/frontity.settings.js
+++ frontity/frontity.settings.js
@@ -1,3 +1,8 @@
+const env = process.env.TERMINUS_ENV;
+const site = process.env.TERMINUS_SITE;
+const url = `https://${env}-${site}.pantheonsite.io`;
+const api = `${url}/wp-json`;
+
 const settings = {
   "name": "frontity",
   "state": {
@@ -45,7 +50,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "api": "https://live-coronavirustualatin.pantheonsite.io/wp-json/"
+          "api": api
         }
       }
     },
